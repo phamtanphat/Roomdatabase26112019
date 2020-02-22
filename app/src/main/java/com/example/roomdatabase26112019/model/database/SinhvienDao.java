5699e1898f4a4c0765f9fa803c8e56172b94da8b
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-02-22 18:36:50 +0700

    dao

---
+++ app/src/main/java/com/example/roomdatabase26112019/model/database/SinhvienDao.java
@@ -0,0 +1,18 @@
+package com.example.roomdatabase26112019.model.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.List;
+
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+@Dao
+public interface SinhvienDao {
+
+    @Query("SELECT * FROM sinhvien")
+    Observable<List<Sinhvien>> getAll();
+
+    @Insert
+    Maybe<Integer> insertSinhvien(Sinhvien... sinhviens);
+}
